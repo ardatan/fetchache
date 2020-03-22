e790466b34f8bd8c5a0c1b00a94441b545b9adc9
Arda TANRIKULU <ardatanrikulu@gmail.com>
2020-03-22 21:22:22 +0300

    v0.0.3

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    rootDir: process.cwd(),
+    globals: {
+      'ts-jest': {
+        diagnostics: false,
+        tsConfig: {
+          module: 'commonjs'
+        }
+      },
+    },
+    reporters: [
+      'default',
+      [
+        'jest-junit',
+        {
+          classNameTemplate: '{classname}',
+          titleTemplate: '{title}',
+          addFileAttribute: 'true',
+        },
+      ],
+    ],
+    modulePathIgnorePatterns: ['<rootDir>/dist'],
+  };
+  
